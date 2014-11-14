9fc986af2ddcf4e48f2e44944a5e9c506186641b
Scott Smereka <scott.smereka@gmail.com>
2014-11-14 14:16:35 -0500

    Fixed issue with merging

--- libs/index.js
+++ libs/index.js
@@ -63,25 +63,58 @@ var defaultConfig = {
  * @param config is the new configuration object.
  */
 var handleCraveConfig = function(config) {
-  CONFIG = (config) ? mergeObjects(config, defaultConfig) : defaultConfig;
+  CONFIG = (config) ? deepPriorityMergeSync(config, defaultConfig) : defaultConfig;
   DEBUG = (CONFIG.debug);
   log = new Log(CONFIG.debug);
 };
 
 /**
- * Combine two object's properties into a single object.  When
- * both objects contain the same property then the obj1's
- * property is copied and the obj2's property is disregarded.
- * @param obj1 is an object to merge and is given priority.
- * @param obj2 is the other object to merge.
- * @returns a single, merged, object.
+ * Merge two objects attributes into a single object.
+ * This will do a deep merge, meaning that if both objects 
+ * contain an attribute that is also an object, then they 
+ * will be merged as well.  This will give all priorty to
+ * the first object, meaning if both objects have the same 
+ * attribute, the first object's value will be preserved
+ * while the second-object's value is not.
  */
-function mergeObjects(obj1, obj2) {
-  for (var key in obj2) {
-    if (obj1[key] === undefined)
-      obj1[key] = obj2[key];
+var deepPriorityMergeSync = function(obj1, obj2) {
+  var result = {};
+
+  // Loop through all the attributes in the first object.
+  for(var key in obj1) {
+    
+    // If obj1's property is an object and not an array
+    if(obj1.hasOwnProperty(key) && obj1[key] !== null && typeof obj1[key] === 'object' && ! (obj1[key] instanceof Array)) {     
+      
+      // And obj2's attribute with the same key is also an object.
+      if(obj2.hasOwnProperty(key) && obj2[key] !== null && typeof obj2[key] === 'object') {
+        // recurse and merge those objects as well.
+        result[key] = deepPriorityMergeSync(obj1[key], obj2[key]);
+      } else {
+        // Otherwise store the object in the result.
+        result[key] = obj1[key];
+      }
+    } else {
+      // If the attribute is not an object, store it in the results.
+      result[key] = obj1[key];
+    }
   }
-  return obj1;
+
+  // Loop through and add all the attributes in object 2 that
+  // are not already in object 1.
+  for(i in obj2) {
+    if(obj2.hasOwnProperty(i)) {
+      // If the attribute is already in the result, skip it.
+      if(i in result) {
+        continue;
+      }
+
+      // Add the new attribute to the result object.
+      result[i] = obj2[i];
+    }
+  }
+
+  return result;
 }
 
 
