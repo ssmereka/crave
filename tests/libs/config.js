b868908dad72e587b8e9081d6a124e6f97fa8c4f
Scott Smereka <scott.smereka@gmail.com>
2015-01-25 17:11:58 -0500

    Added more tests

--- tests/libs/config.js
+++ tests/libs/config.js
@@ -1,13 +1,14 @@
 var crave = require('../../libs/index.js');
 
 var assert = require("assert"),
+    path = require("path"),
     should = require("should"),
     _ = require("lodash");
 
-var exampleConfig = { 
+var defaultConfig = { 
   cache: { 
     enable: false,
-    path: '/home/scott/Documents/oaklabs/crave/data/cache.json' 
+    path: path.resolve(__dirname, "../../data/cache.json")
   },
   debug: false,
   identification: { 
@@ -19,7 +20,7 @@ var exampleConfig = {
 describe('Config', function() {
   var config = crave.getConfig();
 
-  it('config is an object and defined', function() {
+  it('config is an object and defined.', function() {
     assert.equal(_.isObject(config), true);
   });
 
@@ -36,10 +37,64 @@ describe('Config', function() {
   });
 
   it('config default values should be correct.', function() {
-    assert.equal(config["cache"]["enable"], false);
-    assert.equal(config["debug"], false);
-    assert.equal(config["identification"]["type"], 'string');
-    assert.equal(config["identification"]["identifier"], "~>");
+    assert.equal(config["cache"]["enable"], defaultConfig.cache.enable);
+    assert.equal(config["debug"], defaultConfig.debug);
+    assert.equal(config["identification"]["type"], defaultConfig.identification.type);
+    assert.equal(config["identification"]["identifier"], defaultConfig.identification.identifier);
+  });
+
+  describe('default properties can be overridden.  ', function() {
+    var modifiedConfig;
+
+    it('Enable caching for all required files', function() {
+      crave.setConfig({ cache: { enable: true } });
+      modifiedConfig = crave.getConfig();
+      assert.equal(modifiedConfig["cache"]["enable"], true);
+      should(config["cache"]).have.property('path');
+      crave.setConfig(config);
+    });
+
+    it('Disable caching for all required files', function() {
+      crave.setConfig({ cache: { enable: false } });
+      modifiedConfig = crave.getConfig();
+      assert.equal(modifiedConfig["cache"]["enable"], false);
+      should(config["cache"]).have.property('path');
+      crave.setConfig(config);
+    });
+
+    it('Change cache path', function() {
+      var tempPath = path.resolve(__dirname, "../../data/cache1.json");
+      crave.setConfig({ cache: { path: tempPath } });
+      modifiedConfig = crave.getConfig();
+      assert.equal(modifiedConfig["cache"]["enable"], false);
+      should(config["cache"]).have.property('path');
+      assert.equal(modifiedConfig["cache"]["path"], tempPath);
+      crave.setConfig(config);
+    });
+
+    it('Change cache path to an invalid location', function() {
+      crave.setConfig({ cache: { path: undefined } });
+      modifiedConfig = crave.getConfig();
+      assert.equal(modifiedConfig["cache"]["enable"], false);
+      should(config["cache"]).have.property('path');
+      assert.equal(modifiedConfig["cache"]["path"], undefined);
+      crave.setConfig(config);
+    });
+
+    it('Change debug mode', function() {
+      crave.setConfig({ debug: true });
+      modifiedConfig = crave.getConfig();
+      assert.equal(modifiedConfig["debug"], true);
+      crave.setConfig(config);
+    });
+
+    it('Change identification method to filenames', function() {
+      crave.setConfig({ identification: { type: "filename", identifier: "_" } });
+      modifiedConfig = crave.getConfig();
+      assert.equal(modifiedConfig["identification"]["type"], "filename");
+      assert.equal(modifiedConfig["identification"]["identifier"], "_");
+      crave.setConfig(config);
+    });
   });
 
 });
